d18bf4275d42684ff34a507e69ecad7395c8e928
Cristian Gonzalez <crisgonzalez07g@gmail.com>
2023-12-22 00:35:40 -0500

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+import heroBg from './hero-bg.jpg'
+import ctaBg from './cta-bg.jpg'
+import movieDetailBg from './movie-detail-bg.png'
+import footerBg from './footer-bg.jpg'
+import logo from './logo.svg'
+import serviceBg from './service-bg.jpg'
+import topRatedBg from './top-rated-bg.jpg'
+import tvSeriesBg from './tv-series-bg.jpg'
+import upcomingBg from './upcoming-bg.png'
+
+export { heroBg, ctaBg, movieDetailBg, logo, footerBg, serviceBg, topRatedBg, tvSeriesBg, upcomingBg }
